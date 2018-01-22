05119dedf580472a9aefd59f1081c5a3e38f0a9e
Aamen <chen.yes.man@gmail.com>
2018-01-22 15:39:17 +0800

    no message

--- App/Api/Controller/WxpayController.class.php
+++ App/Api/Controller/WxpayController.class.php
@@ -51,8 +51,9 @@ class WxpayController extends Controller{
 		$input->SetNotify_url('https://wechat-shop.zytxgame.com/index.php/Api/Wxpay/notify');
 		$input->SetTrade_type("JSAPI");
 		$input->SetOpenid($openId);
+        echo json_encode($input).__LINE__;
 		$order = \WxPayApi::unifiedOrder($input);
-        echo json_encode($order).__LINE__;
+        // echo json_encode($order).__LINE__;
 		//echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
 		//printf_info($order);
 		$arr = array();
