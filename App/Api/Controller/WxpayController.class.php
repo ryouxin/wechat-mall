1150f70ad0e81cb01dfe4484a8d6efe0752247a2
Aamen <chen.yes.man@gmail.com>
2018-01-31 13:56:24 +0800

    no message

--- App/Api/Controller/WxpayController.class.php
+++ App/Api/Controller/WxpayController.class.php
@@ -72,7 +72,7 @@ class WxpayController extends Controller
         //添加prepay_id
         $prepay_id = array();
         $prepay_id['prepay_id']=$order['prepay_id'];
-        echo json_encode($prepay_id);
+        M('order')->where('prepay_id=0')-find();
         $respnse = M('order')->where('order_sn='.'$pay_sn')->save($prepay_id);
         echo M('order')->getLastSql();
         echo json_encode($respnse);
