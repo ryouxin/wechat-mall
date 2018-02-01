ec4411996afccba906ee8139ab9836aed6f25085
Aamen <chen.yes.man@gmail.com>
2018-02-01 17:35:19 +0800

    no message

--- App/Api/Controller/WxpayController.class.php
+++ App/Api/Controller/WxpayController.class.php
@@ -267,7 +267,7 @@ class WxpayController extends Controller
         $data['touser']=$user_openid;
         $data['template_id']=$template_id;
         $data['form_id']=$form_id;
-        $data['page']='index';
+        // $data['page']='index';
         $data_obj=array();
         $keyword1 = array( "value"=>"$time");
         $data_obj['keyword1']=$keyword1;
