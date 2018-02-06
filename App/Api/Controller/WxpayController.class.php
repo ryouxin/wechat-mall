359d2e3ff27be50a3f7e3645b42ac1f369a6229f
Aamen <chen.yes.man@gmail.com>
2018-02-06 13:41:39 +0800

    no message

--- App/Api/Controller/WxpayController.class.php
+++ App/Api/Controller/WxpayController.class.php
@@ -135,11 +135,11 @@ class WxpayController extends Controller
                     $activation_code = $activation_code;
                     $_activation_code_array = array();
                     foreach ($activation_code as $key => $one) {
-                        array_push($_activation_code_array, $one->CDkey);
-                        // $_index = $key+1;
-                        // $key_val.='激活码'. $_index .': '.$one->CDkey.',';
+                        // array_push($_activation_code_array, $one->CDkey);
+                        $_index = $key+1;
+                        $key_val.='激活码'. $_index .': '.$one->CDkey.',';
                     }
-                    $key_val=json_encode($_activation_code_array);
+                    // $key_val=json_encode($_activation_code_array);
                     // $key_val.=$key_val.' 点击"进入小程序查看"复制激活码。';
                     // $key_val = $product['pro_number'];
                 } else {
