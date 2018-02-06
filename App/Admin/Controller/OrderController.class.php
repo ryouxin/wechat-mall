8953b299fa90dd12651ed3e871ab48474fb2e705
Aamen <chen.yes.man@gmail.com>
2018-02-06 10:47:55 +0800

    no message

--- App/Admin/Controller/OrderController.class.php
+++ App/Admin/Controller/OrderController.class.php
@@ -118,7 +118,7 @@ class OrderController extends PublicController{
 		$order_list = $this->order->where($where)->order('id desc')->limit($Page->firstRow.','.$Page->listRows)->select();
 		foreach ($order_list as $k => $v) {
 			$order_list[$k]['u_name'] = M('user')->where('id='.intval($v['uid']))->getField('name');
-			// $order_list[$k]['products'] = M('')
+			$order_list[$k]['products_name'] = M('order_product')->where('order_id='.intval($v['id']))->getField('name');
 			echo json_encode($order_list[$k]);
 		}
 		//echo $where;
